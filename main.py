22fed8e3eb832bc8191d06cd6c625eb531249832
luna :3 <ellipticobj@gmail.com>
2025-02-08 10:06:43 +0800

    debugged a little

--- main.py
+++ main.py
@@ -13,42 +13,42 @@ def main():
 
     match args[0]:
         case 'init':
-            if len(args) > 2:
+            if len(args) > 3:
                 print("too many arguments for command `lit init`")
+                return
 
             return init()
 
         case 'catfile':
-            args = args[1:]
-            if len(args) < 2:
+            if len(args) < 3:
                 print("command requires two arguments\nusage: lit catfile <type> <hash>")
+                return
 
-            return catfile(args)
+            return catfile(args[1:])
 
         case 'hashobject':
-            args = args[1:]
             if len(args) < 2:
-                print("command requires two arguments\nusage: lit hashobject <filename>")
+                print("command requires one argument\nusage: lit hashobject <filename>")
+                return
             elif len(args) > 3:
                 print("too many arguments for command `lit hashobject`")
+                return
 
-            return hashobject(args)
+            return hashobject(args[1:])
 
         case 'writetree':
-            args = args[1:]
-
-            return writetree(args)
+            return writetree(args[1:])
 
         case 'lstree':
-            args = args[1:]
-            if len(args) != 1:
+            if len(args) != 2:
                 print("command requires one argument\nusage: lit lstree <hash>")
+                return
 
-            return lstree(args)
+            return lstree(args[1:])
 
         case other:
             print(f"unrecognized command {other}")
-
+            return
 
 def parseargs(args):
     flags = []
@@ -57,39 +57,48 @@ def parseargs(args):
     for arg in args:
         if arg.startswith('-'):
             flags.append(arg)
-        elif arg.startswith('--'):
+        else:
             kwargs.append(arg)
 
     return flags, kwargs
 
-def hashfile(path, write=False):
+def hashfile(path, filesize=None, write=False, type="blob"):
     with open(path, "rb") as file:
-        unhashed = f"blob {len(file.read())}\x00{file.read()}".encode('utf-8')
+        content = file.read()
+        filesize = len(content) if not filesize else filesize
+        unhashed = f"{type} {filesize}\x00{content}".encode('utf-8')
 
     hash = hashlib.sha1(unhashed).hexdigest()
 
     if write:
-        os.makedirs(f".git/objects/{hash[:2]}", exist_ok=True)
+        os.makedirs(f".lit/objects/{hash[:2]}", exist_ok=True)
         with open(f".lit/objects/{hash[:2]}/{hash[2:]}", "wb") as file:
-            file.write(zlib.compress(hash.encode()))
+            file.write(zlib.compress(unhashed))
 
     return hash
 
-def init():
-    if os.path.exists(".lit") and os.path.exists(".lit/objects") and os.path.exists(".lit/refs") and os.path.exists(".lit/HEAD"):
-        print("this directory is already a lit repository. \nare you sure you want to continue? (y/n) ", end="")
-        resp = input()
+def decompfile(compressed):
+    decompressed = zlib.decompress(compressed)
+    header, content = decompressed.split(b'\x00', 1)
+    type, size = header.decode().split(" ")
+    return type, size, content.decode()
 
-        if resp.lower() != 'y':
-            print("n")
-            return
-        else:
+def init():
+    if os.path.exists(".lit"):
+        print("this directory is already a lit repository. \nare you sure you want to continue? (y/n) ", end="")
+        resp = input().strip().lower()
+
+        if resp == 'y':
             shutil.rmtree(".lit")
             print("deleted existing lit repository")
+        elif resp == 'n':
+            return
+        else:
+            print("n")
+            return
 
-    os.mkdir(".lit")
-    os.mkdir(".lit/objects")
-    os.mkdir(".lit/refs")
+    os.makedirs(".lit/objects")
+    os.makedirs(".lit/refs")
 
     with open(".lit/HEAD", "w") as file:
         file.write("ref: refs/heads/main\n")
@@ -98,32 +107,30 @@ def init():
     print("current branch is named main") # TODO: make some way for users to switch branches
     return "lit init"
 
-def decompfile(compressed):
-    decompressed = zlib.decompress(compressed)
-    header, content = decompressed.split(b'\x00', 1)
-    type, size = header.decode().split(" ")
-    return type, size, content.decode()
 
 def catfile(args):
     flags, args = parseargs(args)
 
-    if len(args) > 2:
-        print("too many arguments for command `lit catfile`")
-        return
-
-    if len(args) < 2:
-        print("command requires two arguments\nusage: lit catfile <type> <hash>")
-        return
-
-    type = args[0]
-    hash = args[1]
+    # TODO: if there are two args, there shouldnt be any flags, and args should be: type, hash
+    # else: there should be a flag and they should be: -p, hash
 
     # TODO: add support for other object types
-    if "-p" in flags or type == "blob":
+    if "-p" in flags:
         hash = args[1]
         with open(f".lit/objects/{hash[0:2]}/{hash[2:]}", "rb") as file:
             conttype, size, content = decompfile(file.read())
-            print(content)
+            print(content.strip())
+            return
+
+    elif type == "blob":
+        hash = args[1]
+        with open(f".lit/objects/{hash[0:2]}/{hash[2:]}", "rb") as file:
+            conttype, size, content = decompfile(file.read())
+            if conttype != "blob":
+                print(f"object {hash} is not a blob")
+                return
+            print(content.strip())
+            return
 
     else:
         print(f"unrecognized object type {args[1]}\nthis is NOT a full implementation. only blobs are supported for this command.")
@@ -133,7 +140,7 @@ def hashobject(args):
 
     type = 'blob'
 
-    filename = args[1]
+    filename = args[0]
 
     try:
         filesize = os.path.getsize(filename)
@@ -142,9 +149,9 @@ def hashobject(args):
         return
 
     if "-w" in flags:
-        hash = hashfile(filename, write=True)
+        hash = hashfile(filename, filesize, write=True, type=type)
     else:
-        hash = hashfile(filename, write=False)
+        hash = hashfile(filename, filesize, write=False, type=type)
 
     print(hash)
 
@@ -188,11 +195,12 @@ def lstree(args):
 
 def writetree(args):
     flags, args = parseargs(args)
-    path = args[0] if args else "."
+    path = args[0] if args else "./"
 
     if os.path.isfile(path):
         return hashfile(path)
 
+    print(os.path.join(path, x))
     contents = sorted(
         os.listdir(path),
         key=lambda x: x if os.path.isfile(os.path.join(path, x)) else f"{x}/"
@@ -200,20 +208,19 @@ def writetree(args):
     s = b""
 
     for item in contents:
-        if item == ".lit" or item == ".git":
+        if item in [".lit", ".git"]:
             continue
 
         fullpath = os.path.join(path, item)
 
         if os.path.isfile(fullpath):
-            s += f"100644 {item}\x00".encode()
+            hash = hashfile(fullpath)
+            s += f"100644 {item}\x00{hash}".encode()
         else:
-            s += f"40000 {item}\x00".encode()
+            subtreehash = writetree([fullpath])
+            s += f"40000 {item}\x00{subtreehash}".encode()
 
-        hash = int.to_bytes(int(writetree(fullpath), base=16), length=20, byteorder='big')
-        s += hash
-
-    s = f"tree {len(s)}\x00{s}".encode()
+    s = f"tree {len(s)}\x00{s}".encode() + s
     hash = hashlib.sha1(s).hexdigest()
 
     os.makedirs(f".lit/objects/{hash[:2]}", exist_ok=True)
